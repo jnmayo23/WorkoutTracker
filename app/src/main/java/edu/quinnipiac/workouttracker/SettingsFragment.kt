760a35a4e7013158c2efc2b3d3cf3452af94a86c
ecarl1 <113143036+ecarl1@users.noreply.github.com>
2023-04-19 21:54:48 -0400

    d

--- app/src/main/java/edu/quinnipiac/workouttracker/SettingsFragment.kt
+++ app/src/main/java/edu/quinnipiac/workouttracker/SettingsFragment.kt
@@ -1,19 +1,33 @@
 package edu.quinnipiac.workouttracker
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_color, container, false)
+        val redButton = view.findViewById<Button>(R.id.red)
+        val greenButton = view.findViewById<Button>(R.id.green)
+        val blueButton = view.findViewById<Button>(R.id.blue)
+
+        redButton.setOnClickListener { changeBackgroundColor(Color.RED) }
+        greenButton.setOnClickListener { changeBackgroundColor(Color.GREEN) }
+        blueButton.setOnClickListener { changeBackgroundColor(Color.BLUE) }
+
+        return view
     }
 
+    private fun changeBackgroundColor(color: Int) {
+        activity?.window?.decorView?.setBackgroundColor(color)
+    }
 }
